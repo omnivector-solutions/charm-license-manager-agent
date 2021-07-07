752f20fc86fa39eec1a25ce0d8525e6e1dddb22d
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2021-07-07 13:13:07 -0300

    Fix dispatch file

--- src/templates/install_python.sh
+++ src/templates/install_python.sh
@@ -2,13 +2,13 @@
 
 PYTHON38_VERSION="3.8.10"
 
-# install python3.8.10
+echo "Downloading and installing Python $PYTHON38_VERSION"
 
- mkdir ./tmp/
- wget https://www.python.org/ftp/python/${PYTHON38_VERSION}/Python-${PYTHON38_VERSION}.tgz -P ./tmp/
- tar xvf ./tmp/Python-${PYTHON38_VERSION}.tgz -C ./tmp/
- cd ./tmp/Python-${PYTHON38_VERSION}/
- ./configure --enable-optimizations
- make altinstall
- cd ../../
- rm -rf tmp/
+mkdir ./tmp/
+wget https://www.python.org/ftp/python/${PYTHON38_VERSION}/Python-${PYTHON38_VERSION}.tgz -P ./tmp/
+tar xvf ./tmp/Python-${PYTHON38_VERSION}.tgz -C ./tmp/
+cd ./tmp/Python-${PYTHON38_VERSION}/
+./configure --enable-optimizations
+make altinstall
+cd ../../
+rm -rf tmp/
